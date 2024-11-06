c65524599d1a0983342bf6b7f5a01316e6c074cf
Aaron Sollman <unclepong@gmail.com>
2024-11-06 12:31:26 -0600

    testing

--- test/FieldStringTest.php
+++ test/FieldStringTest.php
@@ -6,18 +6,26 @@ use PHPUnit\Framework\TestCase;
 
 class FieldStringTest extends TestCase
 {
-
     public function test__construct()
     {
-        //this tests the constructor of field string, and then tests the __toString() method
-        $fieldString = new FieldString('you fucked my sister');
-        $this->assertEquals('you fucked my sister',(string)$fieldString);
+        /*
+         * this tests the constructor of field string. the result should be blank
+         * because the input is not a valid hex string
+         */
+        $fieldString = new FieldHex('you fucked my sister');
+        $this->assertEquals('',(string)$fieldString);
+
+        /**
+         * This test constructs an object, specifies a max length
+         */
+        $fieldString = new FieldHex('ae01d6ef',4);
+        $this->assertEquals('d6ef',(string)$fieldString);
     }
 
     public function testPad()
     {
         //this function tests the string padding flag
-        $fieldString=new FieldString('0ad0f',8,true);
+        $fieldString=new FieldHex('0ad0f',8,true);
         $this->assertEquals('0000ad0f',(string)$fieldString);
     }
 
@@ -26,11 +34,12 @@ class FieldStringTest extends TestCase
         //this test constructs 2 fields of different types, int and string.
         //the string field will link with and mutate the value of the int field
         //and output the value as a padded string
-        $fieldString=new FieldString('0ad0f',8,true);
+        $fieldString=new FieldHex('0ad0f',8,true);
         $fieldInt=new FieldInt(32,288832);
 
         $fieldString->setLink($fieldInt);
-        $this->assertEquals('0000ad0f',(string)$fieldString);
+        $fieldString = $fieldString->mutateField($fieldInt)->pad(true)->setCharLength(8);
+        $this->assertEquals('00046840',(string)$fieldString);
     }
 
 }
